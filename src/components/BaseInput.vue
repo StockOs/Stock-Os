510fc1cc34a395679ddf0dd19aedf8e2d2b50feb
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-03-30 23:47:22 +0200

    ADD BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input 
+      type="text" 
+      :value="value"
+      v-bind="$attrs"
+      @input="$emit('input', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      require: true
+    },
+    value: {
+      type: [String, Number],
+      require: true,
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <BaseInput label="Prénom" v-model="firstname"/>
+    {{ firstname }}
   </div>
 </template>
 
 <script>
+  import BaseInput from '@/components/BaseInput.vue'
 
-export default {
-  name: 'Home',
-  components: {
+  export default {
+    name: 'Home',
+    components: {
+      BaseInput,
+    },
+    data:() => ({
+      firstname: '',
+    }),
   }
-}
 </script>
