49362339c3fe409e070dd6471fdab08bdec5357d
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-04-01 00:26:34 +0200

    ADD baseCheckbox component

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input 
+      type="checkbox"
+      v-bind="$attrs"
+      :checked="value"
+      @change="$emit('input', $event.target.checked)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      require: true
+    },
+    value: {
+      type: [Boolean, Number],
+      require: false,
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,19 +3,23 @@
     <h1>Home</h1>
     <BaseInput label="Prénom" v-model="firstname"/>
     {{ firstname }}
+    <BaseCheckbox label="maison" v-model="isHome"/>
   </div>
 </template>
 
 <script>
   import BaseInput from '@/components/BaseInput.vue'
+  import BaseCheckbox from '@/components/BaseCheckbox.vue'
 
   export default {
     name: 'Home',
     components: {
       BaseInput,
+      BaseCheckbox
     },
     data:() => ({
       firstname: '',
+      isHome: true,
     }),
   }
 </script>
