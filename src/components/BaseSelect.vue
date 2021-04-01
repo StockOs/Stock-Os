01b6af876d7a06abaa4afcd9f4a7e752999e9f21
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-04-01 23:34:55 +0200

    ADD BaseSelect component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <select 
+      :value="value"
+      @change="$emit('input', $event.target.value)"
+      v-bind="$attrs"
+    >
+      <option 
+        v-for="(item,i) in items" 
+        :key="i"
+        :selected="item === value"
+      >
+        {{ item.category }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    label:{
+      type: String,
+      require: true,
+    },
+    value:{
+      type: String,
+      require: false,
+    },
+    items: {
+      type: Array,
+      require: true,
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,39 @@
     <h1>Home</h1>
     <BaseInput label="Prénom" v-model="firstname"/>
     {{ firstname }}
-    <BaseCheckbox label="maison" v-model="isHome"/>
+    <BaseCheckbox 
+      label="maison" 
+      v-model="isHome"
+    />
+    <BaseSelect 
+      label="Listes d'éléments" 
+      :items="categories" 
+      v-model="categories.category"
+    />
   </div>
 </template>
 
 <script>
   import BaseInput from '@/components/BaseInput.vue'
   import BaseCheckbox from '@/components/BaseCheckbox.vue'
+  import BaseSelect from '@/components/BaseSelect.vue'
 
   export default {
     name: 'Home',
     components: {
       BaseInput,
-      BaseCheckbox
+      BaseCheckbox,
+      BaseSelect,
     },
     data:() => ({
       firstname: '',
       isHome: true,
+      categories: [
+        {id: 1, category: 'fruit'},
+        {id: 2, category: 'viande'},
+        {id: 3, category: 'poisson'},
+        {id: 4, category: 'légume'},
+        ],
     }),
   }
 </script>
