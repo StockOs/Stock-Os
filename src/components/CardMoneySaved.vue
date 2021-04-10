b666d9a3b293e226a0321add569ba63e0f33bd4c
Alexanne Brunold <alexanne.brunold@hetic.net>
2021-04-10 14:54:19 +0200

    ADD Component CardSavedMoney and style

---
+++ src/components/CardMoneySaved.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cardMoneySaved background-white">
+    <h1 class="card-title">Argent économisé ({{ timeUnit }})</h1>
+    <p class="card-text text-padding-top">{{ moneySaved }} €</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    moneySaved: {
+      type: Number,
+    },
+    timeUnit: {
+      type: String,
+      default: '',
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,55 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CardMoneySaved :moneySaved="moneySaved" :timeUnit="timeUnit"/>
     <BaseInput label="Prénom" v-model="firstname" />
     {{ firstname }}
-    <BaseCheckbox
-      label="maison"
-      v-model="isHome"
-    />
+    <BaseCheckbox label="maison" v-model="isHome" />
     <BaseSelect
       label="Listes d'éléments"
       :items="categories"
       v-model="categories.category"
     />
-    <MultiSelect 
+    <MultiSelect
       :options="categories"
       display-option="category"
       value-option="id"
       v-model="ingredient"
     />
+
   </div>
 </template>
 
 <script>
-  import BaseInput from '@/components/BaseInput.vue'
-  import BaseCheckbox from '@/components/BaseCheckbox.vue'
-  import BaseSelect from '@/components/BaseSelect.vue'
-  import MultiSelect from '@/components/MultiSelect.vue'
+import BaseInput from "@/components/BaseInput.vue"
+import BaseCheckbox from "@/components/BaseCheckbox.vue"
+import BaseSelect from "@/components/BaseSelect.vue"
+import MultiSelect from "@/components/MultiSelect.vue"
+import CardMoneySaved from '@/components/CardMoneySaved.vue'
 
-  export default {
-    name: 'Home',
-    components: {
-      BaseInput,
-      BaseCheckbox,
-      BaseSelect,
-      MultiSelect,
-    },
-    data:() => ({
-      firstname: '',
-      isHome: true,
-      categories: [
-        {id: 1, category: 'fruit'},
-        {id: 2, category: 'viande'},
-        {id: 3, category: 'poisson'},
-        {id: 4, category: 'légume'},
-        {id: 5, category: 'saumon'},
-        {id: 6, category: 'pesto'},
-        ],
-      ingredient: [1 , 2, 3, 4],
-    }),
-  }
+export default {
+  name: "Home",
+  components: {
+    BaseInput,
+    BaseCheckbox,
+    BaseSelect,
+    MultiSelect,
+    CardMoneySaved,
+  },
+  data: () => ({
+    firstname: "",
+    isHome: true,
+    categories: [
+      { id: 1, category: "fruit" },
+      { id: 2, category: "viande" },
+      { id: 3, category: "poisson" },
+      { id: 4, category: "légume" },
+      { id: 5, category: "saumon" },
+      { id: 6, category: "pesto" }
+    ],
+    ingredient: [1, 2, 3, 4],
+    moneySaved: 40,
+    timeUnit: 'Mois',
+  })
+}
 </script>
