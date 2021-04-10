0e779b9e19ddb8d4d7ab89c265c3fcc9d26a2578
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-04-10 21:34:56 +0200

    rebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import mainLayout from './mainLayout'
 import login from './login'
 import signUp from './signUp'
+import itemStock from './itemStock'
 
 export default {
   mainLayout,
   login,
   signUp,
+  itemStock
 }
