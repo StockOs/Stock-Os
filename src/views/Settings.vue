5042388df364ddb20a4e8136b2c5294d51b00028
gRAKLECLER <guillaume.rak-lecler@hetic.net>
2021-04-13 01:33:22 +0200

    rebase

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -41,7 +41,7 @@
     <br>
     <hr>
     <div class="account-actions-section">
-      <h2 class="uppercase-text">ACTIONS DU COMPTE</h2>
+      <h2 class="uppercase-text action-title">ACTIONS DU COMPTE</h2>
       <button>Annuler l'abonnement</button>
       <button>Supprimer son compte</button>
     </div>
@@ -53,83 +53,3 @@ export default {
 
 }
 </script>
-
-<style>
-  .main-content{
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-  }
-
-  .account {
-    margin-top: 16px;
-    width: 80%;
-    height: 80%;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .account >  h1 {
-    color: var(--primary-blue);
-    margin-bottom: 16px;
-  }
-
-  .account > div > h2 {
-    color: #a2c3fa;
-  }
-  .account-subscribe-section {
-    display: flex;
-    justify-content: space-between;
-    height: 50%;
-  }
-
-  .account-subscribe-info {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 60%;
-  }
-
-  .account-forfait-section,
-  .account-subscribe-section-firm-name,
-  .account-subscribe-section-firm-mail,
-  .account-subscribe-section-bankInfo {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .account-forfait-section,
-  .account-actions-section,
-  .account-subscribe-section {
-    margin-top: 1rem;
-  }
-
-  .account-forfait-section-info {
-    display: flex;
-    justify-content: space-between;
-    width: 60%;
-  }
-
-  .h2-title {
-    line-height: 2rem;
-  }
-
-  .h3-title{
-    font-weight: 800;
-  }
-
-  .h4-title {
-    font-weight: 800;
-    color: var(--primary-blue);
-  }
-
-  a {
-    text-decoration: none;
-    color: var(--primary-blue);
-  }
-
-  hr {
-    border-top: 1.5px solid var(--primary-blue);
-  }
-</style>
